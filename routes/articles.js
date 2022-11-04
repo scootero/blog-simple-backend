64502ae7e71b12dceb734f28032de8acc9ad9d9a
scootero <61133217+scootero@users.noreply.github.com>
2022-11-04 13:21:48 -0500

    CRUD complete! Update model working but could use work

--- routes/articles.js
+++ routes/articles.js
@@ -6,11 +6,42 @@ router.get("/new", (req, res) => {
   res.render("articles/new", { article: new Article() });
 });
 
-router.get("/:id", async (req, res) => {
-  const article = await Article.findById(req.params.id);
+router.get("/:slug", async (req, res) => {
+  const article = await Article.findOne({ slug: req.params.slug });
   if (article == null) res.redirect("/");
   res.render("articles/show", { article: article });
 });
+
+router.get("/edit/:id", async (req, res) => {
+  const article = await Article.findById(req.params.id);
+  if (article == null) res.redirect("/");
+  res.render("articles/edit", { article: article });
+});
+
+router.delete("/:id", async (req, res) => {
+  // console.log(req.params);
+  await Article.findByIdAndDelete(req.params.id);
+  res.redirect("/");
+});
+
+router.put("/:id", async (req, res) => {
+  console.log(req.params);
+  let article = await Article.findById(req.params.id);
+  console.log(article);
+
+  article.title = req.body.title;
+  article.description = req.body.description;
+  article.markdown = req.body.markdown;
+  try {
+    article = await article.save();
+    res.redirect(`/articles/${article.slug}`);
+  } catch (e) {
+    console.log("IT FAILED!!!! : ", e);
+    // res.send("it failed!");
+    res.render("articles/edit", { article: article });
+  }
+});
+
 router.post("/", async (req, res) => {
   let article = new Article({
     title: req.body.title,
@@ -18,14 +49,17 @@ router.post("/", async (req, res) => {
     markdown: req.body.markdown,
   });
   try {
-    // console.log(article);
     article = await article.save();
-    res.redirect(`/articles/${article.id}`);
+    res.redirect(`/articles/${article.slug}`);
   } catch (e) {
     console.log("IT FAILED!!!! : ", e);
     // res.send("it failed!");
     res.render("articles/new", { article: article });
   }
 });
+
+function saveArticleAndUpdate(path) {
+  return (req, res) => {};
+}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const articleRouter = require("./routes/articles");
+const Article = require("./models/article");
 const app = express();
 const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+
 // app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
 mongoose.connect("mongodb://localhost/blog", {
   useNewUrlParser: true,
@@ -16,14 +20,11 @@ app.set("view engine", "ejs");
 
 app.use("/articles", articleRouter);
 
-app.get("/", (req, res) => {
-  const articles = [
-    {
-      title: "Test Title",
-      createdAt: new Date(),
-      description: "Test description",
-    },
-  ];
+app.get("/", async (req, res) => {
+  // let articles = await Article.find();
+  let articles = await Article.find().sort({
+    createdAt: "desc",
+  });
   res.render("articles/index", { articles: articles });
 });
 
